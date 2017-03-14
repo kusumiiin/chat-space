22d5c23f8d002d4cb7cd641eaf00f1d1edb19384
楠見　朋也 <kusutomo143@gmail.com>
2017-03-14 15:27:56 +0900

    ルーティングとコントローラーの定義

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,16 @@
 class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+  def create
+    @group = Group.create(name: group_params[:name])
+  end
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name)
+  end
 end
