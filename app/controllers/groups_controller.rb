8cbe07b16835369b52b5683dde40e3610e58a822
楠見　朋也 <kusutomo143@gmail.com>
2017-03-14 21:09:16 +0900

    edit routes & controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,26 +1,31 @@
 class GroupsController < ApplicationController
-
+before_action :set_group, only: [:edit, :update]
   def new
     @group = Group.new
   end
 
   def create
-    @group = Group.new(name: group_params[:name])
+    @group = Group.new(group_params)
     if @group.save
-      redirect_to root_path
+      redirect_to root_path, success: "グループの作成が完了しました"
     else
-      redirect_to new_group_path
+      render "new", alert: "グループの作成に失敗しました"
     end
   end
 
-  def edit
+  def set_group
     @group = Group.find(params[:id])
   end
 
+  def edit
+  end
+
   def update
-    group = Group.find(params[:id])
-    group.update(group_params)
-    redirect_to root_path
+    if @group.update(group_params)
+      redirect_to root_path, success: "グループの更新が完了しました"
+    else
+      render "edit", alert: "グループの更新に失敗しました"
+    end
   end
 
 
