1b29e6b323f1e26fb9519113579baeb95a09918f
楠見　朋也 <kusutomo143@gmail.com>
2017-03-15 01:01:52 +0900

    collection_check_boxes

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -30,7 +30,7 @@ class GroupsController < ApplicationController
   private
 
   def group_params
-    params.require(:group).permit(:name)
+    params.require(:group).permit(:name, user_ids: :[])
   end
 
   def set_group
