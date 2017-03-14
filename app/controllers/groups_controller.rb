ae72b300fdc5564257ec10e41e5c172d4cc969ac
楠見　朋也 <kusutomo143@gmail.com>
2017-03-14 19:08:05 +0900

    edit routes & controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,34 @@
 class GroupsController < ApplicationController
+
   def new
     @group = Group.new
   end
+
   def create
-    @group = Group.create(name: group_params[:name])
+    @group = Group.new(name: group_params[:name])
+    if @group.save
+      redirect_to root_path
+    else
+      redirect_to new_group_path
+    end
   end
+
   def edit
     @group = Group.find(params[:id])
   end
 
+  def update
+    group = Group.find(params[:id])
+    group.update(group_params)
+    redirect_to root_path
+  end
+
+
   private
+
   def group_params
     params.require(:group).permit(:name)
   end
+
+
 end
