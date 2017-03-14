459882ce70f751751627477e6fb80435d78c44d3
楠見　朋也 <kusutomo143@gmail.com>
2017-03-14 21:08:07 +0900

    flash_messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  add_flash_types :success, :info, :warning, :danger
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
