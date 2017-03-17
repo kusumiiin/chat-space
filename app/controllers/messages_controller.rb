4e8a5cdd604accf5abc61e91984d788a12d90553
楠見　朋也 <kusutomo143@gmail.com>
2017-03-18 02:00:25 +0900

    ルーティングのネストとmessages#topの記述

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,29 @@
 class MessagesController < ApplicationController
-  def index
+
+  def top
   end
+
+  def index
+    @message = Message.new
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      render :index
+    else
+      render :new, alert: "本文がありません"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body).merge(user_id: current_user.id)
+  end
+
 end
