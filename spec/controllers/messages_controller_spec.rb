3a8c010f18246954f8648cda9be0b0e442fa931d
楠見　朋也 <kusutomo143@gmail.com>
2017-03-23 23:53:47 +0900

    mesages_controller_spec 若干の編集

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,33 +1,34 @@
 require 'rails_helper'
-
 describe MessagesController do
   let(:message) { create(:message, group_id: group.id, user_id: user.id) }
   let(:group) { create(:group) }
-  let(:user) { create(:user) }
   let(:messages) { create_list(:message, 5, group_id: group.id)}
+  let(:user) { create(:user) }
+
+  before do
+    get :index, group_id: group.id
+    login_user user
+  end
 
   describe 'GET #index' do
+
     it "populates an array of messages" do
-      get :index, group_id: group.id
       expect(assigns(:messages)).to eq messages
+    end
+
+    it "populates an array of group" do
       expect(assigns(:group)).to eq group
     end
 
+
     it "renders the :index template" do
-      get :index, group_id: group
       expect(response).to render_template :index
     end
   end
 
   describe 'post #create' do
-    it "create new message" do
-      expect(message).to be_valid
-    end
-
-    it "can't createnew message" do
-      message = build(:message, body: "")
-      message.valid?
-      expect(message.errors[:body]).to include("を入力してください。")
+    it "redirect to the :index template" do
+      expect(response).to render_template :index
     end
   end
 
