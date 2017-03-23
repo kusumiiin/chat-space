a8d29e69cda1a57fa5e37ec1386773a519146a4a
楠見　朋也 <kusutomo143@gmail.com>
2017-03-23 23:52:30 +0900

    deviseを使えるように

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,10 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'devise'
+require File.expand_path("spec/support/controller_macros.rb")
+
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -34,6 +38,12 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
 
   config.include FactoryGirl::Syntax::Methods
+
+  config.include Devise::TestHelpers, :type => :controller
+
+  config.include ControllerMacros, type: :controller
+
+
 
 
   # RSpec Rails can automatically mix in different behaviours to your tests
