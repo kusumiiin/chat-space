deff6d9af91ef8e9b82e17c053bbffc79af39fdd
楠見　朋也 <kusutomo143@gmail.com>
2017-03-23 23:53:22 +0900

    password_confiramation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,12 @@
 FactoryGirl.define do
 
+  pass = Faker::Internet.password(6)
+
   factory :user do
     id { Faker::Number.between(1,10) }
     email { Faker::Internet.email }
-    password { Faker::Internet.password }
+    password pass
+    password_confirmation pass
   end
 
 end
