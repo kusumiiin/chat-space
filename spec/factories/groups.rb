f75b47f47a4c5151f5be3ce02c09dc88e5ab50fc
楠見　朋也 <kusutomo143@gmail.com>
2017-03-25 00:49:45 +0900

    Idは自動で作ってくれそうなので削除

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
 
   factory :group do
-    id { Faker::Number.between(1,10) }
     name { Faker::Lorem.sentence }
   end
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,6 @@ FactoryGirl.define do
   pass = Faker::Internet.password(6)
 
   factory :user do
-    id { Faker::Number.between(1,10) }
     email { Faker::Internet.email }
     password pass
     password_confirmation pass
