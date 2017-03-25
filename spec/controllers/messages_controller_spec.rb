28539d4208ff28ea8757a01ff7a30c9817a62e51
楠見　朋也 <kusutomo143@gmail.com>
2017-03-25 18:14:38 +0900

    とりあえずの修正

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -6,11 +6,13 @@ describe MessagesController do
   let(:user) { create(:user) }
 
   before do
-    get :index, group_id: group.id
     login_user user
   end
 
   describe 'GET #index' do
+    before do
+      get :index, group_id: group.id
+    end
 
     it "populates an array of messages" do
       expect(assigns(:messages)).to eq messages
@@ -20,6 +22,11 @@ describe MessagesController do
       expect(assigns(:group)).to eq group
     end
 
+    it "populates an array of message" do
+      expect(assigns(:message)).to eq message
+
+    end
+
 
     it "renders the :index template" do
       expect(response).to render_template :index
@@ -27,9 +34,30 @@ describe MessagesController do
   end
 
   describe 'post #create' do
-    it "redirect to the :index template" do
-      expect(response).to render_template :index
+
+
+    it "can save message" do
+      expect{
+        post :create, params: { group_id: group.id, message: attributes_for(:message) }
+    }.to change(Message, :count).by(1)
     end
+
+    it "redirect to messages#index" do
+      post :create, params: { group_id: group.id, message: attributes_for(:message)}
+      expect(response).to redirect_to group_messages_path
+    end
+
+    it 'can not save message' do
+      expect(message.save).not_to change(Message, :count).by(1)
+    end
+
+    it 'redirects to messages#index' do
+       expect(response).to redirect_to group_messages_path
+    end
+
   end
 
+
 end
+
+
