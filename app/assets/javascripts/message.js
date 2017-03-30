7342799e9531bb95061b781913b76566ed9fdeb2
楠見　朋也 <kusutomo143@gmail.com>
2017-03-31 00:02:45 +0900

    Js変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,30 +1,43 @@
 $(function() {
   function buildHTML(message) {
-    var html = $('.message__data').append(message__data--text);
-    return html;
+    var html = '<div class="message__data">' +
+                 '<div class="message__data--name' +
+                   'message.user.name' +
+                 '</div>' +
+                 '<div class="message__data--date' +
+                   'message.created_at' +
+                 '</div>' +
+                 '<div class="message__data--text' +
+                   'message.body' +
+                 '</div>' +
+               '</div>';
+     return html;
   }
-  $('form').on('submit', function(e) {
-      e.preventDefault();
-      var textField = $('.type');
-      var message = textField.val();
-      $.ajax({
-        type: 'POST',
-        url: '/messages.json',
-        data: {
-          messages: {
-            content: message
-          }
-        },
-        dataType: 'json'
-      })
-      .done(function(data) {
-        var html = buildHTML(data);
-        $('.content__right').append(html);
-        textField.val('');
-      })
-      .fail(function() {
-        alert('error');
-      });
+  $('#new_message').on('submit', function(e) {
+    e.preventDefault();
+    var form = this;
+    var textField = $('#text_field');
+    var message = textField.val();
+    $.ajax({
+      type: 'post',
+      url: 'messages.json',
+      data: {
+        message: {
+          body: message
+        }
+      },
+      datatype: 'json'
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('#list').append(html);
+      $('#input').prop('disabled', false);
+      var height = $('#list').height();
+      $("#list").scrollTop(height);
+      form.reset();
+    })
+    .fail(function() {
+      alert('error');
     });
+  });
 });
-
