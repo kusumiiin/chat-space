c67388dfad7bfc51bd117813429bf93e492466a8
楠見　朋也 <kusutomo143@gmail.com>
2017-04-14 14:08:30 +0900

    define users#search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+  def search
+    @users = User.where.not(id: current_user.id).where('name LIKE(?)', "%#{params[:name]}%")
+    respond_to do |format|
+      format.json
+    end
+  end
 end
