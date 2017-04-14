98ef095c1a843da864d70f22b434dc2b28c847cc
楠見　朋也 <kusutomo143@gmail.com>
2017-04-14 18:40:09 +0900

    modify js's class

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,25 +1,23 @@
 $(function() {
   function searchedUserList(user) {
     var html = `<div class="chat-group-user">
-                  <p class="chat-group-user__name">
-                    ${user.name}
-                  </p>
-                    <a class="chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name} > 追加 </a>
+                  <p class="chat-group-user__name"> ${user.name} </p>
+                    <a class="chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name} > 追加 </a>
                 </div>`;
     $('#user-search-result').append(html);
   }
 
   function addedUserList(name, id) {
-    var html = `<div class="chat-group-user>"
+    var html = `<div class="chat-group-user">
                   <input name="group[user_ids][]" value="${id}" type="hidden">
                   <p class="chat-group-user__name"> ${name} </p>
-                  <a class="chat-group-user__btn--remove"> 削除 </a>
+                    <a class="chat-group-user__btn chat-group-user__btn--remove"> 削除 </a>
                 </div>`;
     $('#chat-group-users').append(html);
   }
 
   $('#search').on('keyup', function() {
-    var input = $('#chat_group_user').val();
+    var input = $('#search').val();
     var preInput = '';
     if(preInput !== input){
       $.ajax({
