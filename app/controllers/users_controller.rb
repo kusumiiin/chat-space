3415a4da85269c03a982c50b2afa9c3d185b46b2
楠見　朋也 <kusutomo143@gmail.com>
2017-04-15 21:02:16 +0900

    modify search function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def search
-    @users = User.where.not(id: current_user.id).where('name LIKE(?)', "%#{params[:keyword]}%")
+    keyword = params[:keyword]
+    @users = User.where.not(id: current_user.id).where('name LIKE(?)', "%#{keyword}%") if keyword.present?
     respond_to do |format|
       format.json
     end
