025f3f480a9a7997f89ed84278282f86eecf5b1c
楠見　朋也 <kusutomo143@gmail.com>
2017-04-16 00:28:37 +0900

    bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def search
     keyword = params[:keyword]
     @users = User.where.not(id: current_user.id).where('name LIKE(?)', "%#{keyword}%") if keyword.present?
-      respond_to do |format|
-        format.json
-      end
+    respond_to do |format|
+      format.json
+    end
   end
 end
