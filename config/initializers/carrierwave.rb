bcb2335ba1d2e35048845d618b9cc98cca9f994d
楠見　朋也 <kusutomo143@gmail.com>
2017-04-17 22:12:32 +0900

    edit storage

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,6 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage = :fog
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['ACCESS_KEY_ID'],
@@ -13,10 +12,15 @@ CarrierWave.configure do |config|
 
     case Rails.env
     when 'development'
+        config.storage = :fog
         config.fog_directory  = 'chat-space-kusumi'
         config.asset_host = 'https://s3.amazonaws.com/chat-space-kusumi'
     when 'production'
+        config.storage = :fog
         config.fog_directory  = 'chat-space-kusumi'
         config.asset_host = 'https://s3.amazonaws.com/chat-space-kusumis'
+    end
+    when 'test'
+        config.storage = :file
     end
 end
