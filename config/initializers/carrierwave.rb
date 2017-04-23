a2073e08434ac119abbfc9c4310d8b4bf5c8978d
楠見　朋也 <kusutomo143@gmail.com>
2017-04-24 01:42:32 +0900

    edit carrier wave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,10 +4,10 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],
-    region: 'us-east-1'
+    :provider => 'AWS',
+    :aws_access_key_id => ENV['ACCESS_KEY_ID'],
+    :aws_secret_access_key => ENV['SECRET_ACCESS_KEY'],
+    :region => 'us-east-1'
   }
 
     case Rails.env
